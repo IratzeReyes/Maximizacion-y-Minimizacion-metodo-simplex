06b61d7c0ee26cdaaf8af223209114521f856e29
IratzeReyes <andymin1591@gmail.com>
2025-12-04 18:56:54 -0600

    Se temina de crear el codigo para pedir y fuardar los datos de manera organizada y empezamos con la parte de ver si es minimizacion o maximizacion

--- MaximizacionYMinimizacion.java
+++ MaximizacionYMinimizacion.java
@@ -17,7 +17,30 @@ public class MaximizacionYMinimizacion {
         double[][] matrizA = new double[numRestricciones][numVariables];
         double[] matrizB = new double[numRestricciones];
         double[] funcionC = new double[numVariables];
-        
+        //se pide el dato de los coeficientes de la funcion
+        System.out.println("Ingrese los coeficientes de la funcion objetivo: "); //se ingresan los coeficientes primero
+        for (int i = 0; i < numVariables; i++) {
+            System.out.print("Coeficiente " + (i + 1) + " = ");
+            funcionC[i] = lector.nextDouble();
+        }
+        System.out.println("Ingrese las restricciones del sistema AX <= b: "); //luego se ingresan las restricciones
+        for (int i = 0; i < numRestricciones; i++) {
+            System.out.println("Restriccion R" + (i + 1) + ": ");
+            for (int j = 0; j < numVariables; j++) {
+                System.out.print("Ingrese el Coeficiente X" + (j + 1) +  ":");
+                matrizA[i][j] = lector.nextDouble();
+            }
+            System.out.print("Ingrese el valor b" + (i + 1) + ": ");
+            matrizB[i] = lector.nextDouble();
+        }
+        //en esta parte  si es minimizacion, se convierte minimizacion z en maximizacion -z
+        if (opcion == 2) {
+            for (int i = 0; i < funcionC.length; i++) {
+                funcionC[i] = -funcionC[i];
+            }
+        }
+
+
 
     }
 }
